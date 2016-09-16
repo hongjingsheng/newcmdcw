2fd67217f2f44c48d3312c379e2437fe96ec9781
hongjingsheng <iachjs@qq.com>
2016-09-16 13:38:36 +0800

    //

---
+++ app/Conf/config.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+//	//'配置项'=>'配置值'
+//    'DB_TYPE'   => 'mysql', // 数据库类型
+//    'DB_HOST'   => 'localhost', // 服务器地址
+//    'DB_NAME'   => 'thinkphp', // 数据库名
+//    'DB_USER'   => 'root', // 用户名
+//    'DB_PWD'    => '', // 密码
+//    'DB_PORT'   => 3306, // 端口
+//    'DB_PREFIX' => 'think_', // 数据库表前缀
+//    //DSN
+//    'DB_DSN' => 'mysql://root@localhost:3306/thinkphp'
+
+    'APP_GROUP_LIST' => 'Home,Admin',
+    'DEFAULT_GROUP' => 'Home',
+    'APP_GROUP_MODE' => 1,
+    'APP_GROUP_PATH' => 'Modules',
+
+    'TMPL_L_DELIM'=>'<{',
+    'TMPL_R_DELIM'=>'}>',
+
+    'SHOW_PAGE_TRACE' => true,
+);
+?>
---
+++ app/Runtime/Cache/Admin/dba29410dcd288c0e29ede3b94a6463c.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        p{
+            color: #0000FF;
+        }
+    </style>
+</head>
+<body>
+<p>
+    <?php echo ($name); ?>
+</p>
+</body>
+</html>
